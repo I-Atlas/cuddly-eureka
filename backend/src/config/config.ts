e6d128dcb1883bc9b1258c3ff0e12b76dd2dbfae
Alexander Plutalov <team007@azatigroup.com>
2022-05-14 15:39:27 +0400

    add backend template

---
+++ backend/src/config/config.ts
@@ -0,0 +1,15 @@
+export const ENVIRONMENT = process.env.APP_ENV || 'dev'
+export const IS_PRODUCTION = ENVIRONMENT === 'production'
+export const IS_TEST = ENVIRONMENT === 'test'
+export const APP_PORT = Number(process.env.APP_PORT) || 9000
+export const APP_PREFIX_PATH = process.env.APP_PREFIX_PATH || '/'
+export const JWT_SECRET = process.env.JWT_SECRET || 'somerandomkeyherena'
+export const JWT_EXPIRE = process.env.JWT_EXPIRE || '1y'
+export const DB = {
+  USER: process.env.DATABASE_USERNAME,
+  PASSWORD: process.env.DATABASE_PASSWORD,
+  HOST: process.env.DATABASE_HOST,
+  NAME: process.env.DATABASE_NAME,
+  PORT: Number(process.env.DATABASE_PORT) || 27017,
+}
+export const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017/Mocks'
