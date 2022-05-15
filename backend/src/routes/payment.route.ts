a369ec120227071786e1f2c0b9b1b112108a7d79
Alexander Plutalov <team007@azatigroup.com>
2022-05-15 14:05:48 +0400

    Add endpoint for downloading a report for a payment

--- backend/src/routes/payment.route.ts
+++ backend/src/routes/payment.route.ts
@@ -1,6 +1,9 @@
 import { Payment } from '@/models/payment.model';
 import express from 'express';
 import passport from 'passport';
+import { ReturnRequest } from '@/models/returnRequest.model';
+import * as xlsx from 'xlsx';
+import Decimal from 'decimal.js';
 
 const router = express.Router();
 
@@ -22,5 +25,74 @@ router.get('/', passport.authenticate(['jwt', 'anonymous'], { session: false }),
     currentPage: page,
   });
 });
+
+router.get(
+  '/:documentNumber/report',
+  passport.authenticate(['jwt', 'anonymous'], { session: false }),
+  async (req, res, next) => {
+    const allPaymentNumbers = [req.params.documentNumber];
+    const fetchedPayments = await Payment.find({ documentNumber: { $in: allPaymentNumbers } }).exec();
+    const fetchedRequests = await ReturnRequest.find({ documentNumber: { $in: allPaymentNumbers } }).exec();
+
+    const paymentsWithReturnRequests = fetchedPayments.map((payment) => {
+      const requests = fetchedRequests.filter((rr) => rr.documentNumber === payment.documentNumber);
+
+      return {
+        data: payment,
+        returnRequests: requests.sort((a, b) => Number(a.requestNumber) - Number(b.requestNumber)),
+      };
+    });
+
+    // console.log({ all: ['documentNumber', allPaymentNumbers] }, JSON.stringify(paymentsWithReturnRequests, null, 2));
+
+    const reportWorkbook = xlsx.utils.book_new();
+
+    const returnRequestsResult = paymentsWithReturnRequests
+      .map(({ data: payment, returnRequests }) => {
+        const paymentAmount = new Decimal(payment.amount);
+        let returnedAmount = new Decimal(0);
+        const result = [];
+
+        for (const request of returnRequests) {
+          const requestAmount = new Decimal(request.requestAmount);
+
+          let state = null;
+          const requestResult = [payment.documentNumber, request.requestNumber];
+
+          if (request.receiverINN !== payment.payerINN) {
+            state = 'Неверный ИНН';
+          } else if (request.receiverKPP !== payment.payerKPP) {
+            state = 'Неверный КПП';
+          } else if (request.receiverAccount !== payment.payerAccount) {
+            state = 'Неверный Расч. Сч.';
+          } else if (request.receiverBic !== payment.payerBic) {
+            state = 'Неверный БИК';
+          } else if (request.receiverCorr !== payment.payerCorr) {
+            state = 'Неверный Кор Сч.';
+          } else if (requestAmount.plus(returnedAmount).greaterThan(paymentAmount)) {
+            state = 'Неверная сумма';
+          } else {
+            returnedAmount = returnedAmount.plus(requestAmount);
+            state = 'Соответствует';
+          }
+
+          result.push([...requestResult, state]);
+        }
+
+        return result;
+      })
+      .flat();
+
+    const reportSheet = xlsx.utils.aoa_to_sheet(returnRequestsResult);
+
+    xlsx.utils.book_append_sheet(reportWorkbook, reportSheet);
+
+    const fileName = 'Report.xlsx';
+    res.setHeader('Content-disposition', `attachment; filename=${fileName}`);
+    res.setHeader('Content-type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    const wbout = xlsx.write(reportWorkbook, { bookType: 'xlsx', type: 'buffer' });
+    res.send(Buffer.from(wbout));
+  }
+);
 
 export default router;
